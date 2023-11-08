b1a9d062d6274ea2a87054cbe50a44b03606f3de
votha <votha@MSI>
2023-11-08 20:16:18 +0700

    Add project files.

---
+++ BEWebTopZone/Controllers/PicturesController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BEWebTopZone.Models;
+
+namespace BEWebTopZone.Controllers
+{
+    public class PicturesController : Controller
+    {
+        private DBTopZoneEntities db = new DBTopZoneEntities();
+
+        // GET: Pictures
+        public ActionResult Index()
+        {
+            var pictures = db.Pictures.Include(p => p.Product);
+            return View(pictures.ToList());
+        }
+
+        // GET: Pictures/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Picture picture = db.Pictures.Find(id);
+            if (picture == null)
+            {
+                return HttpNotFound();
+            }
+            return View(picture);
+        }
+
+        // GET: Pictures/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProID = new SelectList(db.Products, "ProID", "ProName");
+            return View();
+        }
+
+        // POST: Pictures/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PictureID,ProID,PictureName,PictureDescription,PictureData,"+"UploadImage")] Picture picture)
+        {
+            if (ModelState.IsValid)
+            {
+                //bo sung doan code de gan duong dan anh cho ProImage va luu anh vao thu muc Images tren server
+                if (picture.PictureData != null)
+                {
+                    string path = "~/Content/images/";
+                    string filename = Path.GetFileName(picture.UploadImage.FileName);
+                    picture.PictureData = path + filename;
+                    picture.UploadImage.SaveAs(Path.Combine(Server.MapPath(path), filename));
+                }
+
+
+                // giữ nguyên đoạn code
+                db.Pictures.Add(picture);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProID = new SelectList(db.Products, "ProID", "ProName", picture.ProID);
+            return View(picture);
+        }
+
+        // GET: Pictures/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Picture picture = db.Pictures.Find(id);
+            if (picture == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProID = new SelectList(db.Products, "ProID", "ProName", picture.ProID);
+            return View(picture);
+        }
+
+        // POST: Pictures/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PictureID,ProID,PictureName,PictureDescription,PictureData")] Picture picture)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(picture).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProID = new SelectList(db.Products, "ProID", "ProName", picture.ProID);
+            return View(picture);
+        }
+
+        // GET: Pictures/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Picture picture = db.Pictures.Find(id);
+            if (picture == null)
+            {
+                return HttpNotFound();
+            }
+            return View(picture);
+        }
+
+        // POST: Pictures/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Picture picture = db.Pictures.Find(id);
+            db.Pictures.Remove(picture);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BEWebTopZone/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BEWebTopZone.MvcApplication" Language="C#" %>
